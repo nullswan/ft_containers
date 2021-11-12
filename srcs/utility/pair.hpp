b5d6507739ec611771e1373b2f5ea437b965e0e9
c3b5aw <dev@c3b5aw.dev>
2021-11-12 03:43:28 +0100

    map: init + utility: pair

---
+++ srcs/utility/pair.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pair.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: c3b5aw <dev@c3b5aw.dev>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 03:40:17 by c3b5aw            #+#    #+#             */
+/*   Updated: 2021/11/12 03:42:21 by c3b5aw           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILITY_PAIR_HPP_
+#define UTILITY_PAIR_HPP_
+
+namespace ft {
+template<
+    class T1,
+    class T2
+> struct pair;
+}  // namespace ft
+
+#endif  // UTILITY_PAIR_HPP_

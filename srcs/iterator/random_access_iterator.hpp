3a0f181395390298a9c39dbc3b5dac4199be0ded
c3b5aw <dev@c3b5aw.dev>
2021-11-13 15:38:01 +0100

    iterator: random_access_iterator: done

--- srcs/iterator/random_access_iterator.hpp
+++ srcs/iterator/random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: c3b5aw <dev@c3b5aw.dev>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 18:05:40 by c3b5aw            #+#    #+#             */
-/*   Updated: 2021/11/12 15:56:50 by c3b5aw           ###   ########.fr       */
+/*   Updated: 2021/11/13 15:36:47 by c3b5aw           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,17 +53,26 @@ class random_access_iterator {
 		- X b(a);
 		- b = a;
 	*/
-	random_access_iterator() {
-		_base = NULL;
-	}
+	random_access_iterator()
+		: _base(NULL) {}
 	random_access_iterator(const random_access_iterator &rhs)
-	: _base(rhs._ptr) {}
+		: _base(rhs._base) {}
+	/*
+		no matching conversion for functional-style cast from 
+		'ft::vector<int, std::allocator<int> >::pointer' 
+			(aka 'int *')
+		to
+		'ft::vector<int, std::allocator<int> >::iterator' 
+			(aka 'random_access_iterator<int>')
+	*/
+	explicit random_access_iterator(pointer base)
+		: _base(base) {}
 	random_access_iterator &operator=(const random_access_iterator &rhs) {
 		if (this != &rhs)
 			_base = rhs._base;
 		return *this;
 	}
-	~random_access_iterator() {}
+	virtual ~random_access_iterator() {}
 
 	/*
 		Can be compared for equivalence using the equality/inequality operators
@@ -71,17 +80,30 @@ class random_access_iterator {
 		- a == b
 		- a != b
 	*/
+	bool	operator==	(const random_access_iterator &rhs) const {
+		return (_base == rhs._base);
+	}
+	bool	operator!=	(const random_access_iterator &rhs) const {
+		return _base != rhs._base;
+	}
 
 	/*
 		Can be dereferenced as an rvalue (if in a dereferenceable state).
 		- *a
 		- a->m
 	*/
+	reference operator*	() const {
+		return *_base;
+	}
+	pointer	operator->	() const {
+		return _base;
+	}
 
 	/*
 		For mutable iterators (non-constant iterators):
 		Can be dereferenced as an lvalue (if in a dereferenceable state).
 		- *a = t
+		? a->m = t
 	*/
 
 	/*
@@ -92,6 +114,15 @@ class random_access_iterator {
 		a++
 		*a++
 	*/
+	random_access_iterator &operator++	() {
+		++_base;
+		return *this;
+	}
+	random_access_iterator	operator++	(int) {
+		random_access_iterator temp(*this);
+		++_base;
+		return temp;
+	}
 
 	/*
 		Can be decremented (if a dereferenceable iterator value precedes it).
@@ -99,6 +130,15 @@ class random_access_iterator {
 		- a--
 		- *a--
 	*/
+	random_access_iterator &operator--	() {
+		--_base;
+		return *this;
+	}
+	random_access_iterator operator--	(int) {
+		random_access_iterator temp(*this);
+		--_base;
+		return temp;
+	}
 
 	/*
 		Supports the arithmetic operators + and - between an iterator and an integer value, or subtracting an iterator from another.
@@ -107,6 +147,18 @@ class random_access_iterator {
 		- a - n
 		- a - b
 	*/
+	random_access_iterator 	operator+	(const difference_type &n) const {
+		return random_access_iterator(_base + n);
+	}
+	difference_type 		operator+	(const random_access_iterator &rhs) const {
+		return _base + rhs._base;
+	}
+	random_access_iterator 	operator-	(const difference_type &n) const {
+		return random_access_iterator(_base - n);
+	}
+	difference_type 		operator-	(const random_access_iterator &rhs) const {
+		return _base - rhs._base;
+	}
 
 	/*
 		Can be compared with inequality relational operators (<, >, <= and >=).
@@ -115,19 +167,55 @@ class random_access_iterator {
 		- a <= b
 		- a >= b
 	*/
+	bool	operator<	(const random_access_iterator &rhs) const {
+		return _base < rhs._base;
+	}
+	bool	operator>	(const random_access_iterator &rhs) const {
+		return _base > rhs._base;
+	}
+	bool	operator<=	(const random_access_iterator &rhs) const {
+		return _base <= rhs._base;
+	}
+	bool	operator>=	(const random_access_iterator &rhs) const {
+		return _base >= rhs._base;
+	}
 
 	/*
 		Supports compound assignment operations += and -=
 		- a += n
 		- a -= n
 	*/
+	random_access_iterator &operator+=	(difference_type n) {
+		_base += n;
+		return *this;
+	}
+	random_access_iterator &operator-=	(difference_type n) {
+		_base -= n;
+		return *this;
+	}
 
 	/*
 		Supports the offset dereference operator ([])
 		- a[n]
 	*/
+	reference operator[]	(difference_type n) const {
+		return _base[n];
+	}
 };
 
+template <class Iterator>
+random_access_iterator<Iterator>	operator+	(
+	typename random_access_iterator<Iterator>::difference_type n,
+	const random_access_iterator<Iterator> &rhs) {
+	return n + rhs;
+}
+
+template <class Iterator>
+random_access_iterator<Iterator>	operator-	(
+	typename random_access_iterator<Iterator>::difference_type n,
+	const random_access_iterator<Iterator> &rhs) {
+	return n - rhs;
+}
 }  // namespace ft
 
 #endif  // ITERATOR_RANDOM_ACCESS_ITERATOR_HPP_

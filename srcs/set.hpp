abb2d3d3f0cce75d6bae2f79d3293daba45b7b22
c3b5aw <dev@c3b5aw.dev>
2021-12-26 18:44:37 +0100

    update set template

--- srcs/set.hpp
+++ srcs/set.hpp
@@ -6,17 +6,28 @@
 /*   By: c3b5aw <dev@c3b5aw.dev>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 22:50:22 by c3b5aw            #+#    #+#             */
-/*   Updated: 2021/11/10 18:09:22 by c3b5aw           ###   ########.fr       */
+/*   Updated: 2021/12/26 18:44:10 by c3b5aw           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SET_HPP_
 #define SET_HPP_
 
+/*
+	Link:
+	https://www.cplusplus.com/reference/set/set/
+*/
+
+#include <memory>		// provides std::allocator
+#include <functional>   // provides std::less
+
 namespace ft {
+template <
+	class T,							// set::key_type/value_type
+	class Compare = std::less<T>,		// set::key_compare/value_compare
+	class Alloc = std::allocator<T>		// set::allocator_type
+>
 class set {
- public:
- private:
 };
 }  // namespace ft
 

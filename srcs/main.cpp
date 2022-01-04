ccef15c743fbce5b958ee90c591c830c85094ac8
c3b5aw <dev@c3b5aw.dev>
2022-01-04 17:44:10 +0100

    re-add all tests

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: c3b5aw <dev@c3b5aw.dev>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 13:21:50 by c3b5aw            #+#    #+#             */
-/*   Updated: 2021/12/30 20:03:11 by c3b5aw           ###   ########.fr       */
+/*   Updated: 2022/01/04 17:32:57 by c3b5aw           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #include "set_test.hpp"
 
 int	main() {
-	// ft_test::vector();
-	// ft_test::stack();
-	ft_test::set();  // ToDo: put after map
+	ft_test::vector();
+	ft_test::stack();
 	ft_test::map();
+	ft_test::set();
 }
